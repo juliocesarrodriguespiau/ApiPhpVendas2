6bcf53ca9313b47081d7f2c8a3fc0236e971c8be
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2022-02-05 12:35:22 -0300

    ajustes inicais

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+//MYSQL
+const DBDRIVE = "mysql";
+const DBHOST = "localhost";
+const DBNAME = "vendas";
+const DBUSER = "root";
+const DBPASS = "root";
---
+++ public_html/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'C:\xampp\htdocs\ApiPhpVendas2\vendor\autoload.php';
+
+echo "julio!!!!!!!!!!!!!!";
